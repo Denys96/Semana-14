22dc51ee9f627af54ffb9881a9d973bdc05435d5
Denys Alexander Hernandez Maravilla <68742852+Denys96@users.noreply.github.com>
2021-05-30 08:06:08 -0600

    Agenda Amigos Couch db

---
+++ app/src/main/java/com/ugb/miprimerproyecto/utilidades.java
@@ -0,0 +1,6 @@
+package com.ugb.miprimerproyecto;
+
+public class utilidades {
+    static String url_consulta = "http://192.168.100.52:5984/db_agenda/_design/agenda/_view/mi-agenda";
+   static String url_mto = "http://192.168.56.52:5984/db_agenda/";
+}
